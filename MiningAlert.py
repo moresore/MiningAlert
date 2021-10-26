b50f67fb432a5f64d22e0922cb127e9250a40f55
moresore <wiwat.man@hotmail.com>
2021-10-26 14:12:24 +0700

    Update MiningAlert.py
    Add exception when can not connect to internet and can not connect to trex server

--- MiningAlert.py
+++ MiningAlert.py
@@ -1,12 +1,16 @@
 import json
 from urllib.request import urlopen
+from urllib.error import URLError
 import requests
 import time
 
 def Line(msg,token):
     url = 'https://notify-api.line.me/api/notify'
     headers = {'content-type':'application/x-www-form-urlencoded','Authorization':'Bearer '+token}
-    r = requests.post(url, headers=headers, data = {'message':msg})
+    try:
+        r = requests.post(url, headers=headers, data = {'message':msg})
+    except :
+        print("Can not connect to Internet")
 
 while(1):
 
@@ -17,25 +21,32 @@ while(1):
 
 
     url = "http://127.0.0.1:4067/summary"
-    response = urlopen(url)
-    data_json = json.loads(response.read())
-    gpu = data_json['gpu_total']
 
-    # find hashrate
-    f = data_json["hashrate"]/1000000
-    s = str(f) + " MH/s"
+    try:
+        response = urlopen(url)
+        data_json = json.loads(response.read())
+        gpu = data_json['gpu_total']
 
-    # find worker name's
-    pairs = data_json.items()
-    for key, value in pairs:
-        wks = str.find(str(value),"'worker':")
-        if wks != -1 :
-            wkf = str.find(str(value), "'}")
-            wkc = str(value)[wks+11:wkf]
+        # find hashrate
+        f = data_json["hashrate"]/1000000
+        s = str(f) + " MH/s"
 
-    msg = wkc + " running with " + str(gpu) + " GPUs " + s
-    Line(msg,tokenL)
-    print(msg)
+        # find worker name's
+        pairs = data_json.items()
+        for key, value in pairs:
+            wks = str.find(str(value),"'worker':")
+            if wks != -1 :
+                wkf = str.find(str(value), "'}")
+                wkc = str(value)[wks+11:wkf]
+
+        msg = wkc + " running with " + str(gpu) + " GPUs " + s
+        Line(msg,tokenL)
+        print(msg)
+
+    except URLError :
+        print("Trex is not running")
+        Line("Trex is not running",tokenL)
+
+
     print("Waiting for next " + TimDel + " seconds")
     time.sleep(float(TimDel))
-
